70005cd218a454fbe3b6c2cd19975d5729361752
Raymond Yeh <ray@geek.sg>
2020-05-20 23:51:12 +0800

    fix: lint

--- src/handlers/getRides/getRides.ts
+++ src/handlers/getRides/getRides.ts
@@ -7,7 +7,7 @@ const PAGE_SIZE = 20;
 
 export const getRides = async (request: Request) => {
   if (request.query.page && isNaN(Number(request.query.page)))
-  throw createHttpError(400, "page must be a number", { error_code: "VALIDATION_ERROR" });
+    throw createHttpError(400, "page must be a number", { error_code: "VALIDATION_ERROR" });
   const page = Number(request.query.page) || 0;
   if (page < 0) throw createHttpError(400, "page must be >= 0", { error_code: "VALIDATION_ERROR" });
   const rides = await all(`SELECT * FROM Rides LIMIT ${PAGE_SIZE} OFFSET ?`, [page * PAGE_SIZE]);
