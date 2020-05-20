ddcebdea97b6f276d49abe21435c8abcf8c41205
Raymond Yeh <ray@geek.sg>
2020-05-20 23:49:27 +0800

    chore: test for getRides

--- src/handlers/getRides/getRides.test.ts
+++ src/handlers/getRides/getRides.test.ts
@@ -1,8 +1,11 @@
-import { expect } from "chai";
+import { expect, use } from "chai";
+import chaiAsPromised from "chai-as-promised";
 import { stub } from "sinon";
 import * as database from "../../database";
-import { getRide } from "./getRides";
+import { getRides } from "./getRides";
 import rides from "../../../fixtures/rides.json";
+
+use(chaiAsPromised);
 
 const allStub = stub(database, "all");
 
@@ -13,12 +16,31 @@ describe("getRides", () => {
 
   it("should send the correct query", async () => {
     allStub.resolves(rides);
-    const res = await getRide({ query: { page: 5 } } as any);
+    const res = await getRides({ query: { page: 5 } } as any);
     expect(res).equal(rides);
     expect(allStub.args[0]).deep.equals(["SELECT * FROM Rides LIMIT 20 OFFSET ?", [100]]);
   });
 
-  it("should throw when page is NaN", async () => {});
-  it("should throw when page is negative", async () => {});
-  it("should throw when results is empty", async () => {});
+  it("should throw when page is NaN", async () => {
+    const res = getRides({ query: { page: "foo" } } as any);
+    await expect(res).rejectedWith(/page must be a number/);
+  });
+
+  it("should throw when page is negative", async () => {
+    const res = getRides({ query: { page: -2 } } as any);
+    await expect(res).rejectedWith(/page must be >= 0/);
+  });
+
+  it("should throw when results is empty", async () => {
+    allStub.resolves([]);
+    const res = getRides({ query: {} } as any);
+    await expect(res).rejectedWith(/Could not find any rides/);
+  });
+
+  it("should default to page zero", async () => {
+    allStub.resolves(rides);
+    const res = await getRides({ query: {} } as any);
+    expect(res).equal(rides);
+    expect(allStub.args[0]).deep.equals(["SELECT * FROM Rides LIMIT 20 OFFSET ?", [0]]);
+  });
 });
--- src/handlers/getRides/getRides.ts
+++ src/handlers/getRides/getRides.ts
@@ -5,13 +5,15 @@ import { all } from "../../database";
 
 const PAGE_SIZE = 20;
 
-export const getRide = async (request: Request) => {
+export const getRides = async (request: Request) => {
+  if (request.query.page && isNaN(Number(request.query.page)))
+  throw createHttpError(400, "page must be a number", { error_code: "VALIDATION_ERROR" });
   const page = Number(request.query.page) || 0;
-  if (isNaN(page) || page < 0) throw createHttpError(400, "page must be a number", { error_code: "VALIDATION_ERROR" });
+  if (page < 0) throw createHttpError(400, "page must be >= 0", { error_code: "VALIDATION_ERROR" });
   const rides = await all(`SELECT * FROM Rides LIMIT ${PAGE_SIZE} OFFSET ?`, [page * PAGE_SIZE]);
   if (rides.length === 0)
     throw createHttpError(404, "Could not find any rides", { error_code: "RIDES_NOT_FOUND_ERROR" });
   return rides;
 };
 
-export const getRideHandler = handlerBoundary(getRide);
+export const getRidesHandler = handlerBoundary(getRides);
