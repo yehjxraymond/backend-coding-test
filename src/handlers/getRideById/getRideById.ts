8a32f0adb4269f618cdf32c10f23460753ea9742
Raymond Yeh <ray@geek.sg>
2020-05-21 20:56:18 +0800

    fix: getRideById types

--- src/handlers/getRideById/getRideById.ts
+++ src/handlers/getRideById/getRideById.ts
@@ -2,10 +2,10 @@ import createHttpError from "http-errors";
 import { Request } from "express";
 import { handlerBoundary } from "../handlerBoundary";
 import { all } from "../../database";
-import { Ride } from "../../types";
+import { RideEntry } from "../../types";
 
 export const getRideById = async (req: Request) => {
-  const rides = await all<Ride[]>("SELECT * FROM Rides WHERE rideID=?", [req.params.id]);
+  const rides = await all<RideEntry[]>("SELECT * FROM Rides WHERE rideID=?", [req.params.id]);
   if (rides.length === 0)
     throw createHttpError(404, "Could not find any rides", { error_code: "RIDES_NOT_FOUND_ERROR" });
   return rides;
