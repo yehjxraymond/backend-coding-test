7a84b64dcb7757e2a7dea4d26a59a1ee35e96c28
Raymond Yeh <ray@geek.sg>
2020-05-23 15:44:51 +0800

    fix: sql injection on POST rides

--- src/handlers/createRide/createRide.ts
+++ src/handlers/createRide/createRide.ts
@@ -1,15 +1,18 @@
 import { Request } from "express";
 import { handlerBoundary } from "../handlerBoundary";
 import { run, all } from "../../database";
+import { validateRideInput } from "./validate";
 import { RideEntry } from "../../types";
 
 export const createRide = async (req: Request) => {
-  const { startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle } = req.body;
+  const { startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle } = validateRideInput(req.body);
+
+  const values = [startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle];
 
   const { lastID } = await run(
-    `INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (${startLat}, ${startLong}, ${endLat}, ${endLong}, "${riderName}", "${driverName}", "${driverVehicle}")`
+    "INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    values
   );
-
   const rows = all<RideEntry[]>("SELECT * FROM Rides WHERE rideID = ?", lastID);
   return rows;
 };
