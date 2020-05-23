3c8aa13a28120246a4d47c8ebb769a29720f21a6
Raymond Yeh <ray@geek.sg>
2020-05-23 15:09:59 +0800

    fix: sql injection for GET rides/{id}

--- src/handlers/getRideById/getRideById.ts
+++ src/handlers/getRideById/getRideById.ts
@@ -1,12 +1,19 @@
 import createHttpError from "http-errors";
 import { Request } from "express";
+import Joi, { assert } from "@hapi/joi";
 import { handlerBoundary } from "../handlerBoundary";
 import { all } from "../../database";
 import { RideEntry } from "../../types";
 
 export const getRideById = async (req: Request) => {
   const { id } = req.params;
-  const rides = await all<RideEntry[]>(`SELECT * FROM Rides WHERE rideID=${id}`);
+  assert(
+    id,
+    Joi.number()
+      .required()
+      .min(1)
+  );
+  const rides = await all<RideEntry[]>("SELECT * FROM Rides WHERE rideID=?", [id]);
   if (rides.length === 0)
     throw createHttpError(404, "Could not find any rides", { error_code: "RIDES_NOT_FOUND_ERROR" });
   return rides;
